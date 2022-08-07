9b71599a67fd12c5a04a8c7be5ee96a8f06ccc77
kitoyjami <weurbanor@unitru.edu.pe>
2022-08-07 18:13:11 -0500

    new function added

--- index.js
+++ index.js
@@ -3,8 +3,58 @@ const express = require('express')
 // guarda funciones o metodos
 const app = express()
 
+
+app.use(express.json())
+
 app.get('/', (req, res) => {
-    console.log('req',req)
-    console.log('res',res)
+    res.status(200).json({message: "hello world",
+    data:[
+        {
+            "id" :1,
+            "name" : "eddy",
+            "lastName": "Urbano Ramos",
+            "email": "eddyuga@gmail.com"
+        },
+        {
+            "id" :2,
+            "name" : "Keyko",
+            "lastName": "Cruz Zegarra",
+            "email": "kekito@gmail.com"
+        }
+    ]
 })
+})
+
+app.post ('/', (req,res) => {
+    const {name, lastName, email }= req.body
+    console.log("body", req.body)
+    res.status(200).json( {
+        message: 'user created succesfully',
+        data:[
+            {
+                "name":name,
+                "lastName":lastName,
+                "email":email
+            }
+        ]
+    })
+
+})
+
+app.delete ('/:id', (req,res) => {
+    res.status(200).json({
+        message:"User deleted succesfully",
+        data:""
+    })
+})
+
+app.put ('/', (req,res) => {
+    
+})
+
+
+app.listen(3001, ()=>{
+    console.log('Aplication is running')
+})
+
 
